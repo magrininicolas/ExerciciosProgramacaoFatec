1c0cde09ef6b0d4d254a5c7c770cb7d4bc4afccf
Nicolas <magrininicolas@outlook.com>
2023-04-27 14:25:27 -0300

    Somatorio.c foi modificado

--- MatematicaDiscreta/Programacao/Somatorio.c
+++ MatematicaDiscreta/Programacao/Somatorio.c
@@ -4,6 +4,18 @@
 #include <stdio.h>  
 #include <math.h>  
 
+float calculaAproxPI(int n) {
+  float pi_soma = 0;
+  for(int i = 0; i <= n; i++){
+    pi_soma += 8.0/((4 * i + 1) * (4 * i + 3));
+  }
+  return pi_soma;
+}
+
+float calculaErroAbs(float pi) {
+  return fabs(M_PI - pi);
+}
+
 int main(void) {
   float pi_soma = 0, erro;
   int n;
@@ -11,11 +23,9 @@ int main(void) {
   printf("Insira o valor desejado para N: ");
   scanf("%d", &n);
 
-  for(int i = 0; i <= n; i++){
-    pi_soma += 8.0/((4 * i + 1) * (4 * i + 3));
-  }
-  
-  erro = fabs(M_PI - pi_soma);
+  pi_soma = calculaAproxPI(n);
+
+  erro = calculaErroAbs(pi_soma);
 
   printf("O valor aproximado de pi foi: %.6f\n", pi_soma);
   printf("O erro absoluto em relação ao valor correto foi: %.6f\n", erro);
