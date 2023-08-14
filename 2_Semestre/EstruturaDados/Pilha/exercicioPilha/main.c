ffb7cd27146be9d8d05b99ec1bd9e30833ecc102
Nicolas <magrininicolas@outlook.com>
2023-08-14 10:54:41 -0300

    Modified 2_Semestre/EstruturaDados

--- 2_Semestre/EstruturaDados/Pilha/exercicioPilha/main.c
+++ 2_Semestre/EstruturaDados/Pilha/exercicioPilha/main.c
@@ -30,52 +30,51 @@ typedef struct {
 
 BARALHO baralho;
 int sorteado = 0;
+CARTA carta;
+// CARTA cartasSorteadas[MAX];
 
 int menu();
 void embaralhar();
 int geraCartaRand();
 BOOL sortear(CARTA *carta);
+char *getNaipe(int naipe);
 
 int main() {
   int opcao = NAO_SELECIONADO;
-  CARTA carta;
 
   while (opcao != SAIR) {
     opcao = menu();
 
     switch (opcao) {
-      case EMBARALHAR:
-        embaralhar();
-        break;
+    case EMBARALHAR:
+      embaralhar();
+      break;
 
-      case SORTEAR:
-        if (sortear(&carta)) {
-          carta.valor = carta.valor % 13 + 1;
+    case SORTEAR:
+      if (sortear(&carta)) {
+        carta.valor = carta.valor % 13 + 1;
 
-          if (carta.valor == 1) {
-            printf("Carta sorteada: A de %s\n",
-              carta.naipe == PAUS ? "Paus" : carta.naipe == OUROS ? "Ouros" : carta.naipe == COPAS ? "Copas" : "Espadas");
-          } else if (carta.valor == 11) {
-            printf("Carta sorteada: J de %s\n",
-              carta.naipe == PAUS ? "Paus" : carta.naipe == OUROS ? "Ouros" : carta.naipe == COPAS ? "Copas" : "Espadas");
-          } else if (carta.valor == 12) {
-            printf("Carta sorteada: Q de %s\n",
-              carta.naipe == PAUS ? "Paus" : carta.naipe == OUROS ? "Ouros" : carta.naipe == COPAS ? "Copas" : "Espadas");
-          } else if (carta.valor == 13) {
-            printf("Carta sorteada: K de %s\n",
-              carta.naipe == PAUS ? "Paus" : carta.naipe == OUROS ? "Ouros" : carta.naipe == COPAS ? "Copas" : "Espadas");
-          } else {
-            printf("Carta sorteada: %d de %s\n", carta.valor,
-              carta.naipe == PAUS ? "Paus" : carta.naipe == OUROS ? "Ouros" : carta.naipe == COPAS ? "Copas" : "Espadas");
-          }
+        char *naipe = getNaipe(carta.naipe);
+
+        if (carta.valor == 1) {
+          printf("\nCarta sorteada: A de %s\n", naipe);
+        } else if (carta.valor == 11) {
+          printf("\nCarta sorteada: J de %s\n", naipe);
+        } else if (carta.valor == 12) {
+          printf("\nCarta sorteada: Q de %s\n", naipe);
+        } else if (carta.valor == 13) {
+          printf("\nCarta sorteada: K de %s\n", naipe);
+        } else {
+          printf("\nCarta sorteada: %d de %s\n", carta.valor, naipe);
         }
-        break;
+      }
+      break;
 
-      case SAIR:
-        break;
+    case SAIR:
+      break;
 
-      default:
-        printf("Opcao invalida\n");
+    default:
+      printf("Opcao invalida\n");
     }
   }
 
@@ -101,8 +100,11 @@ void embaralhar() {
   while (baralho.topo < (MAX - sorteado)) {
     int valor = geraCartaRand();
 
-    while (valores[valor - 1]) {
-      valor = geraCartaRand();
+    for (int i = 0; i < baralho.topo; i++) {
+      if (baralho.cartas[i].valor == valor) {
+        valor = geraCartaRand();
+        i = -1;
+      }
     }
 
     valores[valor - 1] = TRUE;
@@ -117,23 +119,44 @@ void embaralhar() {
     } else {
       baralho.cartas[baralho.topo].naipe = ESPADAS;
     }
+
     baralho.topo++;
   }
-  printf("Topo: %d\n", baralho.topo);
+  printf("\nTopo: %d\n", baralho.topo);
 }
 
 int sortear(CARTA *carta) {
   if (baralho.topo == 0) {
-    printf("Baralho vazio\n");
+    printf("\nBaralho vazio\n");
     return FALSE;
   }
 
   baralho.topo--;
+  // cartasSorteadas[sorteado] = *carta;
   sorteado++;
   *carta = baralho.cartas[baralho.topo];
   return TRUE;
 }
 
-int geraCartaRand() {
-  return 1 + rand() % MAX;
+int geraCartaRand() { return 1 + rand() % MAX; }
+
+int geraCarta(BARALHO baralho) { srand(time(NULL)); }
+
+char *getNaipe(int naipe) {
+  switch (naipe) {
+  case PAUS:
+    return "Paus";
+    break;
+  case COPAS:
+    return "Copas";
+    break;
+  case OUROS:
+    return "Ouros";
+  case ESPADAS:
+    return "Espadas";
+    break;
+  default:
+    return "Desconhecido";
+  }
 }
+
